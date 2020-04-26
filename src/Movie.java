b0185356402b2973b6eda086ab989ce3988718e3
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-04-26 18:12:17 +0200

    database schema

---
+++ src/Movie.java
@@ -0,0 +1,22 @@
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int movieID;
+    private String title;
+    private String director;
+    private String script;
+    private String description;
+    private int unitsInStock;
+    private double pricePerUnit;
+    private Set<Loan> loans;
+    private Set<LoanHist> loansHist;
+}
